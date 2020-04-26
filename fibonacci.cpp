f6b38a6d9d43e5c170e7a8fc7ced4473c8502e9c
jakoud <64370531+jakoud@users.noreply.github.com>
2020-04-26 18:56:44 +0200

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+//fibonacci
+int main()
+{
+	int F[n];
+	int n, i;
+	F[0]=1;
+	F[1]=1;
+	printf("witaj w programie!\n");
+	printf("podaj numer wyrazu ciagu fibonacciego:\t");
+	scanf("%d", &n);
+	for (i=2;i<=n;i++)
+		A[i]+=A[i-1]+A[i-2];
+	
+	printf ("twoj wyraz to:\t%d", A[n]);
+}
